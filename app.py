ce7ee04d542f2a7d0b6d05b4d6ca73a891bca973
chiaoro <surry318@gmail.com>
2025-04-07 16:31:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -307,7 +307,10 @@ def home():
 
     
     if text == "主選單":
-        line_bot_api.reply_message(event.reply_token, get_main_menu())
+        try:
+            line_bot_api.reply_message(event.reply_token, get_main_menu())
+        except LineBotApiError:
+            line_bot_api.push_message(user_id, get_main_menu())
     elif text == "門診調整服務":
         line_bot_api.reply_message(event.reply_token, get_submenu("門診調整選單", clinic_buttons))
     elif text == "支援醫師服務":
