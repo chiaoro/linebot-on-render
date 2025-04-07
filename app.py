7e65413a6b2287e72106529930c105446735048b
chiaoro <surry318@gmail.com>
2025-04-07 14:20:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -238,12 +238,10 @@ def handle_message(event):
 
 
     if text == "主選單":
-    try:
-        line_bot_api.reply_message(event.reply_token, get_main_menu())
-    except LineBotApiError:
-        line_bot_api.push_message(user_id, get_main_menu())
-
-
+       try:
+           line_bot_api.reply_message(event.reply_token, get_main_menu())
+       except LineBotApiError:
+           line_bot_api.push_message(user_id, get_main_menu())
     
     elif text == "門診調整服務":
         line_bot_api.reply_message(event.reply_token, get_submenu("門診調整選單", clinic_buttons))
