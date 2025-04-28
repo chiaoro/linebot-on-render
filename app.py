1e1857d4473a14dc0329498d51a47b8f0fe93589
chiaoro <surry318@gmail.com>
2025-04-28 16:43:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -237,6 +237,4 @@ def home():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port) == "__main__":
-    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
