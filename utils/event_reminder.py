b0ec9d9c4d07dfd539d4290e483c9a689a9828e9
chiaoro <surry318@gmail.com>
2025-04-28 20:42:33 +0800

    Update event_reminder.py

--- utils/event_reminder.py
+++ utils/event_reminder.py
@@ -1,26 +1,27 @@
 # utils/event_reminder.py
 
-from utils.line_push import push_text_to_user
+import os
+import json
 import gspread
-import os, json
-from oauth2client.service_account import ServiceAccountCredentials
-from datetime import datetime
 from dotenv import load_dotenv
+from oauth2client.service_account import ServiceAccountCredentials
+from utils.line_push import push_text_to_user
+from datetime import datetime, timedelta
 
 load_dotenv()
 
+# Google Sheets認證
 SCOPE = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds_dict = json.loads(os.getenv("GOOGLE_CREDENTIALS", "{}"))
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, SCOPE)
 gc = gspread.authorize(creds)
 
 def send_important_event_reminder():
-    # 今天要推播明天的重要會議
-    today = datetime.now()
-    tomorrow = today + timedelta(days=1)
+    tomorrow = datetime.now() + timedelta(days=1)
     tomorrow_str = tomorrow.strftime("%Y-%m-%d")
-
+    
+    # 發送重要會議前一天提醒
     push_text_to_user(
         user_id=os.getenv("All_doctor_group_id"),
-        text=f"📣【重要會議提醒】\n明天({tomorrow_str})有重要院務會議，請大家準時出席唷！"
+        text=f"📣【重要會議提醒】\n明天 ({tomorrow_str}) 有重要院務會議，請大家準時出席！"
     )
