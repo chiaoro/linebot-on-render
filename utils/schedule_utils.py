9d7676fdd42cfd0b99f5c3e5788cf907ddc817f6
chiaoro <surry318@gmail.com>
2025-04-29 05:42:15 +0800

    Update schedule_utils.py

--- utils/schedule_utils.py
+++ utils/schedule_utils.py
@@ -1,45 +1,13 @@
-import os
-import re
+# utils/schedule_utils.py
+
+import gspread
 from datetime import datetime
+
+# ✅ Google Sheets 認證
 from utils.google_auth import get_gspread_client
-from utils.line_push import push_text_to_user
 
-
-
-
-def handle_submission(name, off_text):
+def handle_submission(name, off_days):
     gc = get_gspread_client()
-    sched = gc.open_by_url(os.getenv("DOCTOR_SCHEDULE_SHEET_URL")).sheet1
-
-    headers = sched.row_values(1)
-    date_cols = {int(day): idx for idx, day in enumerate(headers[1:], start=2) if day.isdigit()}
-
-    raw_parts = re.split(r"[，,\s\n]+", off_text)
-    days = set()
-    for part in raw_parts:
-        part = part.strip().replace("5/", "").replace("05/", "")
-        if "-" in part:
-            try:
-                start, end = map(int, part.split("-"))
-                days.update(range(start, end + 1))
-            except:
-                continue
-        elif part.isdigit():
-            days.add(int(part))
-
-    existing = sched.get_all_values()
-    name_col = [row[0] for row in existing]
-    if name in name_col:
-        row_idx = name_col.index(name) + 1
-        row_data = sched.row_values(row_idx)
-        for d in days:
-            if d in date_cols:
-                col_idx = date_cols[d]
-                if len(row_data) < col_idx or row_data[col_idx - 1] == "":
-                    sched.update_cell(row_idx, col_idx, "off")
-    else:
-        new_row = [name] + ["" for _ in range(len(headers) - 1)]
-        for d in days:
-            if d in date_cols:
-                new_row[date_cols[d] - 1] = "off"
-        sched.append_row(new_row)
+    sheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1fHf5XlbvLMd6ytAh_t8Bsi5ghToiQHZy1NlVfEG7VIo/edit").worksheet("外科醫師休假登記表")
+    today = datetime.now().strftime("%Y/%m/%d")
+    sheet.append_row([today, name, off_days])
