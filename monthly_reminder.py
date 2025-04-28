60efe7d006dd4f2d19d795da61e1493f37c71d9c
chiaoro <surry318@gmail.com>
2025-04-29 05:37:11 +0800

    Create monthly_reminder.py

---
+++ monthly_reminder.py
@@ -0,0 +1,18 @@
+import os, json, gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from dotenv import load_dotenv
+from utils.line_push import push_text_to_group
+
+load_dotenv()
+
+def send_monthly_fixed_reminders():
+    SCOPE = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds_dict = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, SCOPE)
+    gc = gspread.authorize(creds)
+    sheet = gc.open_by_url(os.getenv("FIXED_REMINDER_SHEET_URL")).worksheet("固定日期推播")
+    records = sheet.get_all_records()
+    today = datetime.now().strftime("%Y/%m/%d")
+    for record in records:
+        if record.get("日期") == today and record.get("推播狀態") != "已推播":
+            push_text_to_group(os.getenv("All_doctor_group_id"), record["推播內容"])
