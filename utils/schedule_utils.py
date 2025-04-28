1425a3d19ea6b26522d7fdd468c49e06f4ec36e9
chiaoro <surry318@gmail.com>
2025-04-29 05:43:43 +0800

    Update schedule_utils.py

--- utils/schedule_utils.py
+++ utils/schedule_utils.py
@@ -2,12 +2,29 @@
 
 import gspread
 from datetime import datetime
+from oauth2client.service_account import ServiceAccountCredentials
+import os, json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SCOPE = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+CREDS_DICT = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+CREDS = ServiceAccountCredentials.from_json_keyfile_dict(CREDS_DICT, SCOPE)
+GC = gspread.authorize(CREDS)
+
+
+# 使用者對照表（固定網址）
+MAPPING_SHEET_URL = "https://docs.google.com/spreadsheets/d/1fHf5XlbvLMd6ytAh_t8Bsi5ghToiQHZy1NlVfEG7VIo/edit"
+
 
 # ✅ Google Sheets 認證
 from utils.google_auth import get_gspread_client
 
 def handle_submission(name, off_days):
-    gc = get_gspread_client()
-    sheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1fHf5XlbvLMd6ytAh_t8Bsi5ghToiQHZy1NlVfEG7VIo/edit").worksheet("外科醫師休假登記表")
-    today = datetime.now().strftime("%Y/%m/%d")
-    sheet.append_row([today, name, off_days])
+    """接收醫師請假表單，處理並記錄"""
+    doc = GC.open_by_url(MAPPING_SHEET_URL)
+    sheet = doc.worksheet("排班表")  # 你可以換成要寫入的分頁名稱
+
+    values = [name] + off_days
+    sheet.append_row(values)
