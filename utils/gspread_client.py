d7dfe76fc9e690ef943ac093956befabaf72c6ef
chiaoro <surry318@gmail.com>
2025-04-29 09:53:59 +0800

    Create gspread_client.py

---
+++ utils/gspread_client.py
@@ -0,0 +1,10 @@
+import os, json, gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SCOPE = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds_dict = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, SCOPE)
+gc = gspread.authorize(creds)
