a3372fa58cdb521f634499e286d389e8439cf926
chiaoro <surry318@gmail.com>
2025-05-01 15:09:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ from utils.daily_night_fee_reminder import send_night_fee_reminders
 from utils.user_binding import handle_user_binding
 from utils.user_binding import send_bind_start_flex, ask_for_name, confirm_binding, ensure_user_id_exists, user_states
 from utils.user_binding import ensure_user_id_exists, handle_user_binding
-
+from utils.date_utils import expand_date_range
 
 
 
@@ -170,41 +170,6 @@ def handle_message(event):
         if step == 1:
             date_input = user_msg
             session["step"] = 2
-    
-            # ✅ 區間展開函式
-            def expand_date_range(date_str):
-                from datetime import datetime, timedelta
-                import re
-    
-                today_year = datetime.now().year
-                results = []
-    
-                parts = re.split(r"[、,]", date_str)
-    
-                for part in parts:
-                    part = part.strip()
-    
-                    if "-" in part:
-                        start_str, end_str = part.split("-")
-                        if "/" not in end_str:
-                            start_month = int(start_str.split("/")[0])
-                            start_day = int(start_str.split("/")[1])
-                            end_day = int(end_str)
-                            end_month = start_month
-                        else:
-                            start_month, start_day = map(int, start_str.split("/"))
-                            end_month, end_day = map(int, end_str.split("/"))
-    
-                        start_date = datetime(today_year, start_month, start_day)
-                        end_date = datetime(today_year, end_month, end_day)
-    
-                        while start_date <= end_date:
-                            results.append(start_date.strftime("%-m/%-d"))
-                            start_date += timedelta(days=1)
-                    else:
-                        results.append(part)
-    
-                return results
     
             expanded_dates = expand_date_range(date_input)
     
