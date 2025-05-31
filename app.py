35a0f981dcaa6ed136d8ef19112b3958bfcd7cb4
chiaoro <surry318@gmail.com>
2025-05-31 23:51:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ from utils.night_shift_fee_generator import generate_night_fee_docs
 from handlers.duty_handler import handle_duty_message
 from utils.message_guard import should_ignore_message, handle_direct_command
 from handlers.meeting_leave_handler import handle_meeting_leave
+from utils.session_manager import user_sessions
 
 
 
