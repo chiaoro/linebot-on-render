dead1bfbdccc0422eee52f4db96c2c54845fa602
chiaoro <surry318@gmail.com>
2025-06-01 06:22:56 +0800

    Update doctor_info.py

--- utils/doctor_info.py
+++ utils/doctor_info.py
@@ -11,7 +11,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_dict(google_creds, scope)
 gc = gspread.authorize(creds)
 
 def get_doctor_info(sheet_url, user_id):
-    sheet = gc.open_by_url(sheet_url).worksheet("使用者對照表")
+    sheet = gc.open_by_url(sheet_url).worksheet("UserMapping")
     records = sheet.get_all_records()
     for row in records:
         if row["user_id"] == user_id:
