fde582bc4aa335d180ac286ea93c11560138d84f
chiaoro <surry318@gmail.com>
2025-06-01 06:17:37 +0800

    Delete utils/night_fee_handler.py

--- utils/night_fee_handler.py
+++
@@ -1,8 +0,0 @@
-# handlers/night_fee_handler.py
-from linebot.models import TextSendMessage
-
-def handle_night_fee(event, user_id, text, line_bot_api):
-    if text == "夜點費申請":
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="請輸入日期…"))
-        return True
-    return False
