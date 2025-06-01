c0a2a8fff865da1bb019ab22b4377da88bedf0f0
chiaoro <surry318@gmail.com>
2025-06-01 09:17:26 +0800

    Update session_manager.py

--- utils/session_manager.py
+++ utils/session_manager.py
@@ -1,2 +1,12 @@
 # utils/session_manager.py
+
 user_sessions = {}
+
+def get_session(user_id):
+    return user_sessions.get(user_id, {})
+
+def set_session(user_id, data):
+    user_sessions[user_id] = data
+
+def clear_session(user_id):
+    user_sessions.pop(user_id, None)
