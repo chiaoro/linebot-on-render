df935a399b2a5b3ad21d54f19ec928a856b27370
chiaoro <surry318@gmail.com>
2025-06-01 09:36:37 +0800

    Update date_parser.py

--- utils/date_parser.py
+++ utils/date_parser.py
@@ -19,3 +19,22 @@ def parse_dates_from_text(text):
         else:
             dates.append(part.strip())
     return dates
+
+
+def expand_date_range(text):
+    dates = []
+    parts = re.split(r"[、,，\s]+", text)
+    for part in parts:
+        if "-" in part:
+            try:
+                start, end = part.split("-")
+                m = int(start.split("/")[0])
+                start_day = int(start.split("/")[1])
+                end_day = int(end.split("/")[1])
+                for d in range(start_day, end_day + 1):
+                    dates.append(f"{m}/{d}")
+            except:
+                continue
+        else:
+            dates.append(part.strip())
+    return dates
