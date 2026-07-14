ea8b33c7dd97ec75368d2d0f6b3f784e55a29fac
chiaoro <surry318@gmail.com>
2026-07-14 10:02:02 +0800

    Add files via upload

---
+++ utils/command_texts.py
@@ -0,0 +1,23 @@
+# Texts that start or navigate to a top-level feature.
+# If a user taps one of these while another flow is waiting for input,
+# the old flow should release the message so the intended feature can handle it.
+MENU_COMMANDS = {
+    "主選單",
+    "門診調整服務",
+    "值班調整服務",
+    "支援醫師服務",
+    "新進醫師服務",
+    "加班申請服務",
+    "其他表單服務",
+    "我要調診",
+    "我要休診",
+    "我要代診",
+    "我要加診",
+    "值班調換",
+    "值班代理",
+    "支援醫師調診單",
+    "加班申請",
+    "院務會議請假",
+    "夜點費申請",
+    "查詢醫師資料（限制使用）",
+}
